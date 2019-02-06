7309a4904e06c0283e8f95d80f975b020f3eb063
safiyehnik <47315371+safiyehnik@users.noreply.github.com>
2019-02-06 19:18:25 +0330

    connect to thinger.io

---
+++ webserver.test1.ino
@@ -0,0 +1,25 @@
+#include<ESP8266WiFi.h>
+#include<ThingerESP8266.h>
+#define USERNAME "*******"
+#define DEVICE_ID "****"
+#define DEVICE_CREDENTIAL "******"
+#define SSID  "******"
+#define SSID_PASSWORD "******"
+
+ThingerESP8266 thing(USERNAME,DEVICE_ID,DEVICE_CREDENTIAL);
+
+
+
+
+void setup() {
+
+thing.add_wifi(SSID,SSID_PASSWORD);
+
+}
+
+void loop() {
+ thing.handle();
+
+}
+
+
